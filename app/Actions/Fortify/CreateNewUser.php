faf5e5ec8b078c26ffe519409a8f8e22a0977fdf
BurritoCosmico <albertomoralesflores12@gmail.com>
2022-09-19 17:39:15 -0500

    Mi cuarto Commit Register Update

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,7 +22,9 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'pais' => ['required', 'string', 'max:255'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
@@ -30,6 +32,8 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
+            'last_name' => $input['last_name'],
+            'pais' => $input['pais'],
             'password' => Hash::make($input['password']),
         ]);
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,78 @@
 <x-authentication-layout>
-    <h1 class="text-3xl text-slate-800 font-bold mb-6">{{ __('Create your Account') }} ✨</h1>
+    <h1 class="text-3xl text-slate-800 font-bold mb-6 text-center">{{ __('CREAR MI CUENTA') }} </h1>
     <!-- Form -->
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <div class="space-y-4 ">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+                        Nombre
+                    </label>
+                    <input required  id="name" name="name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="John">
+                </div>
+                 <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="last_name">
+                        Apellido
+                    </label>
+                    <input required  id="last_name" name="last_name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="Doe">
+                </div>
+          </div>
+        </div>
 
-            <div class="w-full  px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name" for="email" value="{{ __('Correo Electrónico:') }}">
-                   Correo Electrónico:
-                </label>
-                <input id="email" type="email" name="email" :value="old('email')" required autofocus  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="correo@correo.com">
-            </div>
 
+        <div class="space-y-4 ">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full  px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="email">
+                        Correo Electrónico
+                    </label>
+                    <input required name="email"  id="email" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder=" correo@correo.com">
+                </div>
+              </div>
+        </div>
 
+        <div class="space-y-4">
+            <div class="mt-4">
+                <div class="relative">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="pais">
+                        Seleccionar tu país
+                    </label>
+                    <select  name="pais"  required class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
+                      <option value="México">México</option>
+                      </select>
 
-            <div class="w-full  px-3">
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"  for="password" value="{{ __('Password') }}">
-                   Contraseña:
-                </label>
-                <input id="password" type="password" name="password" required autocomplete="current-password"   class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" placeholder="**************">
-            </div>
+                  </div>
+               </div>
+        </div>
 
-    </div>
 
 
         <div class="space-y-4">
-            <div>
-                <x-jet-label for="name">{{ __('Full Name') }} <span class="text-rose-500">*</span></x-jet-label>
-                <x-jet-input id="name" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            </div>
+            <div class="flex flex-wrap -mx-3 mb-6 mt-5">
+                                <div class="w-full md:w-1/2 px-3">
+                                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password">
+                                        Password
+                                    </label>
+                                    <input required autocomplete="new-password" id="password" name="password" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="password" placeholder="*********">
+                                </div>
+                                <div class="w-full md:w-1/2 px-3">
+                                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="password_confirmation">
+                                        Confirma Password
+                                    </label>
+                                    <input  required autocomplete="new-password" id="password_confirmation" name="password_confirmation"  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="password" placeholder="*********">
+                                </div>
+                            </div>
 
-            <div>
-                <x-jet-label for="email">{{ __('Email Address') }} <span class="text-rose-500">*</span></x-jet-label>
-                <x-jet-input id="email" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <div>
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div>
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
         </div>
-        <div class="flex items-center justify-between mt-6">
-            <div class="mr-1">
-                <label class="flex items-center" name="newsletter" id="newsletter">
-                    <input type="checkbox" class="form-checkbox" />
-                    <span class="text-sm ml-2">Email me about product news.</span>
-                </label>
-            </div>
-            <x-jet-button>
-                {{ __('Sign Up') }}
-            </x-jet-button>
+
+        <div class="space-y-4">
+            <button type="submit" class="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg
+                                   px-4 py-3 mt-6">Registrarme </button>
+
         </div>
+
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-6">
                     <label class="flex items-start">
