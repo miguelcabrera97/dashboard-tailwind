25c5bc4c6b526fd6c59ecd7b2f22b230ba384427
BurritoCosmico <albertomoralesflores12@gmail.com>
2022-09-19 17:17:51 -0500

    Mi segundo Commit

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-2 w-full max-w-9xl mx-auto">
-        
+
         <!-- Welcome banner -->
         <x-dashboard.welcome-banner />
 
@@ -26,26 +26,39 @@
                     </svg>
                     <span class="hidden xs:block ml-2">Add View</span>
                 </button>
-                
+
             </div> --}}
 
         </div>
-        
+
         <!-- Cards -->
         <div class="grid grid-cols-1 sm:grid-cols-3 gap-9">
 
             <!-- Line chart (Acme Plus) -->
             {{-- <x-dashboard.dashboard-card-01 :dataFeed="$dataFeed" /> --}}
-            
-                <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm text-center  inline-block align-middle">01</div>
+
+                <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm text-center  inline-block align-middle">
+
+                    <div class=" grid grid-rows-1 text-center">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-1/2 h-1/2 text-center">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
+                          </svg>
+
+
+                    </div>
+
+                </div>
+
+
                 <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm">02</div>
                 <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm">03</div>
                 <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm">04</div>
                 <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm">05</div>
                 <div class="relative hover:scale-110 transition duration-200 bg-blue-200 p-6 h-60 rounded-lg shadow-sm">06</div>
-            
+
             <!-- Line chart (Acme Advanced) -->
-             {{-- <x-dashboard.dashboard-card-02/> 
+             {{-- <x-dashboard.dashboard-card-02/>
              <x-dashboard.dashboard-card-03/>
              <x-dashboard.dashboard-card-04/>
              <x-dashboard.dashboard-card-05/>
@@ -81,7 +94,7 @@
 
             <!-- Card (Recent Activity) -->
             {{-- <x-dashboard.dashboard-card-12 /> --}}
-            
+
             <!-- Card (Income/Expenses) -->
             {{-- <x-dashboard.dashboard-card-13 /> --}}
 
