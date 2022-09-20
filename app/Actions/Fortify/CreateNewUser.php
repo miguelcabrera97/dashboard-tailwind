b078df6d21f02351017822848b11b9d48e64cc6b
miguelcabrera97 <migue122747@gmail.com>
2022-09-20 16:53:35 -0500

    stripe

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use Illuminate\Support\Facades\DB;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -29,6 +30,38 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
+        $client = new \GuzzleHttp\Client();
+        // DUDA ACCOUNT CREATE
+        $response = $client->request('POST', 'https://api.duda.co/api/accounts/create', [
+            'body' => '{"account_type":"CUSTOMER","account_name":"'.$input['email'].'","first_name":"'.$input['name'].'" ,"last_name":"'.$input['last_name'].'"}',
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Basic MTczMDA3ZDhlNTpUUWU5Wm5WeDB2dE4=',
+                'Content-Type' => 'application/json',
+            ],
+            ]);
+            
+        $stripe = new \Stripe\StripeClient(
+            'sk_test_51LZk7pIouA9z8SYyfOAHSEm9opwyaipP01qRyhkiTnsw7Ue4a3GtNopuzDKyMzzrelXDmDEKcliXaSW0lI8f9euv00XJ8VrToP'
+          );
+
+         $customer = $stripe->customers->create(array(
+            'email' => $input['email'],
+            'name'=>$input['name'],
+            'description'=> 'Cliente Social Conecta',
+            //'currecy' => 'mxn'
+          ));
+
+        DB::table('clientes')->insert([
+            'id_stripe' => $customer->id,
+            'name' => $input['name'],
+            'email' => $customer->email,
+            'descripcion'=>$customer->description,
+            //'currency' => $customer->currency,
+            'invoice_prefix' => $customer->invoice_prefix,
+            ]);
+
+        
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
