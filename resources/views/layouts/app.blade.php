b2cda278e4f07d5987493738fb341ab434be1117
miguelcabrera97 <migue122747@gmail.com>
2022-09-21 15:19:07 -0500

    favicon y logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link rel="icon" href="{{ asset('images/SocialConecta.ico') }}">
+
         <!-- Styles -->
         @livewireStyles
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,7 +9,8 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
+        <link rel="icon" href="{{ asset('images/SocialConecta.ico') }}">
+        
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
