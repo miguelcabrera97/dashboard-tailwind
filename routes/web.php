b6e3c761691e2776549e3654d6f8fb827538b35d
BurritoCosmico <albertomoralesflores12@gmail.com>
2022-12-19 13:55:34 -0600

    Soporte Submit

--- routes/web.php
+++ routes/web.php
@@ -76,3 +76,5 @@ Route::post('/despublish',[SitesController::class,'despublish'])->name('despubli
 Route::get('/soporte',[SoporteController::class,'sitios'])->name('soporte');
 Route::get('/tickets',[SoporteController::class,'ticket'])->name('tickets');
 
+
+Route::post("/supportform",[SoporteController::class,'InsertDataSupport'])->name('supportform');
