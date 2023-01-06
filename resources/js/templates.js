28b16e320d5364b97cd9890bd1480a725b6a4bc4
BurritoCosmico <albertomoralesflores12@gmail.com>
2023-01-06 17:11:59 -0600

    Viva Morena

--- resources/js/templates.js
+++ resources/js/templates.js
@@ -4,7 +4,7 @@
 
  const expresion  =
  {
-    nombre_sitio : /^[a-zA-Z0-9]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre_sitio : /^[a-zA-Z0-9]{7,}$/, // Letras, numeros, guion y guion_bajo
  }
 
  const campo =
@@ -14,12 +14,31 @@
 
  const ValidacionModal = (e) =>
  {
-
+    switch(e.target.name)
+    {
+      case "nombre" :
+        validar_entrada_modal(expresion.nombre_sitio, e.target,'nombrar_sitio_modal');
+      break;
+    }
  }
 
  const validar_entrada_modal = (expresion,entrada , campo) =>
  {
+    if(expresion.test(entrada.value)){
+		document.getElementById('validacion_ok').classList.remove('Oculto');
+        document.getElementById('validacion_not').classList.add('Oculto');
 
+        let entrada_input = document.getElementById(`${campo}`).value;
+        document.getElementById('okname').innerHTML = entrada_input ;
+
+		campo[campo] = true;
+	} else {
+		document.getElementById('validacion_not').classList.remove('Oculto');
+        document.getElementById('validacion_ok').classList.add('Oculto');
+        let entrada_input = document.getElementById(`${campo}`).value;
+        document.getElementById('notname').innerHTML = entrada_input ;
+		campo[campo] = false;
+	}
  }
 
 
